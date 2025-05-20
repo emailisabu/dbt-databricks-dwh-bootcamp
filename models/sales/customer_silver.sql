5b21a429f3322c4d8a3f4df62d4056bad9a11564
mohi <mohideen-abdulkadir.peermohammed@electrolux.com>
2025-05-20 12:04:36 +0000

    add Cphone column

--- models/sales/customer_silver.sql
+++ models/sales/customer_silver.sql
@@ -2,7 +2,8 @@ select
     C_CUSTKEY,
     C_NAME,
     C_NATIONKEY,
-    C_ACCTBAL
+    C_ACCTBAL,
+    C_PHONE
 from {{ source('tpch', 'customer_bronze') }}
 where C_CUSTKEY is not null
 and C_NATIONKEY != 21
